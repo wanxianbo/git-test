fd957dab0f2a8c0440efa45f420472a7a7ec6677
PrideWan <1036700294@qq.com>
2020-03-25 17:06:48 +0800

    第一次将gittest提交

---
+++ gittest/src/main/java/com/wxb/Git.java
@@ -0,0 +1,14 @@
+package com.wxb;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("git1");
+        System.out.println("git1");
+        System.out.println("git1");
+        System.out.println("git1");
+        System.out.println("git1");
+        System.out.println("git1");
+        System.out.println("git1");
+        System.out.println("git1");
+    }
+}
